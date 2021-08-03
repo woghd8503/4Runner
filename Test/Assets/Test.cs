cd6f603a80bb400746f683b85902226691e0610f
DokyungHwang <ghkdehrud@gmail.com>
2021-08-03 23:00:57 +0900

    Move Cube

--- Test/Assets/Test.cs
+++ Test/Assets/Test.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public string test = "¿¬½À";
+    public float speed = 1f;
+
     private void Awake()
     {
         test = "3333";
@@ -14,10 +16,13 @@ public class Test : MonoBehaviour
     {
         test = "2222";
         Debug.Log(test);
+
+        Debug.Log(this.gameObject);
     }
 
     void Update()
     {
-        
+        this.gameObject.transform.Translate(new Vector3(0f, 0f, 1f * speed* Time.deltaTime));
+        Debug.Log(this.gameObject.transform);
     }
 }
